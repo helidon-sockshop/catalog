e75f730255f698c752e148aa2aa86ef78833117f
Aleksandar Seovic <aleks@seovic.com>
2020-03-10 02:15:26 -0400

    Test cleanup to allow server to be started more than once

--- catalog-core/src/test/java/io/helidon/examples/sockshop/catalog/CatalogResourceIT.java
+++ catalog-core/src/test/java/io/helidon/examples/sockshop/catalog/CatalogResourceIT.java
@@ -4,6 +4,7 @@ import io.helidon.microprofile.server.Server;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -16,12 +17,22 @@ import static org.hamcrest.Matchers.is;
  * Integration tests for {@link io.helidon.examples.sockshop.catalog.CatalogResource}.
  */
 public class CatalogResourceIT {
+    private static Server SERVER;
+
     /**
      * This will start the application on ephemeral port to avoid port conflicts.
      * We can discover the actual port by calling {@link io.helidon.microprofile.server.Server#port()} method afterwards.
      */
-    private static final Server SERVER = Server.builder().port(0).build().start();
+    @BeforeAll
+    static void startServer() {
+        // disable global tracing so we can start server in multiple test suites
+        System.setProperty("tracing.global", "false");
+        SERVER = Server.builder().port(0).build().start();
+    }
 
+    /**
+     * Stop the server, as we cannot have multiple servers started at the same time.
+     */
     @AfterAll
     static void stopServer() {
         SERVER.stop();
--- catalog-mysql/src/test/java/io/helidon/examples/sockshop/carts/jpa/JpaCatalogRepositoryIT.java
+++ catalog-mysql/src/test/java/io/helidon/examples/sockshop/carts/jpa/JpaCatalogRepositoryIT.java
@@ -5,18 +5,28 @@ import io.helidon.examples.sockshop.catalog.CatalogRepositoryTest;
 import io.helidon.microprofile.server.Server;
 
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 
 /**
  * Integration tests for {@link JpaCatalogRepository}.
  */
 public class JpaCatalogRepositoryIT extends CatalogRepositoryTest {
+    private static Server SERVER;
 
     /**
-     * Starting server on ephemeral port in order to be able to get the
-     * fully configured repository from the CDI container.
+     * This will start the application on ephemeral port to avoid port conflicts.
+     * We can discover the actual port by calling {@link io.helidon.microprofile.server.Server#port()} method afterwards.
      */
-    private static final Server SERVER = Server.builder().port(0).build().start();
+    @BeforeAll
+    static void startServer() {
+        // disable global tracing so we can start server in multiple test suites
+        System.setProperty("tracing.global", "false");
+        SERVER = Server.builder().port(0).build().start();
+    }
 
+    /**
+     * Stop the server, as we cannot have multiple servers started at the same time.
+     */
     @AfterAll
     static void stopServer() {
         SERVER.stop();
