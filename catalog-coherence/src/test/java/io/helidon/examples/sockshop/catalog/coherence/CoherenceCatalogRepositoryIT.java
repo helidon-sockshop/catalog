7119b28b30656d4a4f2cdb88ca4cecd3e03e10d8
Aleksandar Seovic <aleks@seovic.com>
2020-06-11 21:25:01 -0400

    Fix broken tests

--- catalog-coherence/src/test/java/io/helidon/examples/sockshop/catalog/coherence/CoherenceCatalogRepositoryIT.java
+++ catalog-coherence/src/test/java/io/helidon/examples/sockshop/catalog/coherence/CoherenceCatalogRepositoryIT.java
@@ -9,14 +9,38 @@ package io.helidon.examples.sockshop.catalog.coherence;
 
 import io.helidon.examples.sockshop.catalog.CatalogRepository;
 import io.helidon.examples.sockshop.catalog.CatalogRepositoryTest;
+import io.helidon.microprofile.server.Server;
 
 import com.tangosol.net.CacheFactory;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 
 /**
  * Tests for Coherence repository implementation.
  */
 class CoherenceCatalogRepositoryIT extends CatalogRepositoryTest {
+    protected static Server SERVER;
+
+    /**
+     * This will start the application on ephemeral port to avoid port conflicts.
+     * We can discover the actual port by calling {@link io.helidon.microprofile.server.Server#port()} method afterwards.
+     */
+    @BeforeAll
+    static void startServer() {
+        // disable global tracing so we can start server in multiple test suites
+        System.setProperty("tracing.global", "false");
+        SERVER = Server.builder().port(0).build().start();
+    }
+
+    /**
+     * Stop the server, as we cannot have multiple servers started at the same time.
+     */
+    @AfterAll
+    static void stopServer() {
+        SERVER.stop();
+    }
+
     public CatalogRepository getCatalogRepository() {
-        return new CoherenceCatalogRepository(CacheFactory.getCache("socks")).loadData();
+        return SERVER.cdiContainer().select(CatalogRepository.class).get();
     }
 }
